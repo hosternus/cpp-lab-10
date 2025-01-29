58e8ffa5c46256d398b528e23cefbbfa2d6bfb25
hosternus <90009071+hosternus@users.noreply.github.com>
2025-01-29 08:11:11 +0300

    e

--- main.cpp
+++ main.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 
+using namespace std;
 
+template <typename T>
+class PriorityQueue {
+    private:
+
+        struct QueueElem {
+            T item;
+            size_t priority;
+        };
+
+        QueueElem* arr = nullptr;
+
+    public:
+        PriorityQueue() {
+            this->arr = new QueueElem[1000];
+        }
+
+        ~PriorityQueue() {
+            delete[] arr;
+        }
+
+        // pop();
+        // push();
+        // peek();
+        // size();
+
+};
+
+
+int main(void) {
+
+
+
+    return 0;
+}
